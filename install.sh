50433b8d920838e990dfc778d9a620a834485dac
Mohamed Shabeer K P <mohammedshabeerkp@gmail.com>
2026-06-06 20:19:56 +0000

    minor

--- install.sh
+++ install.sh
@@ -3,26 +3,24 @@
 echo "[+] Updating system..."
 pkg update -y && pkg upgrade -y
 
-echo "[+] Setup storage..." 
-termux-setup-storage -y
+echo "[+] Installing required packages..."
+pkg install runit termux-api -y
 
-echo "[+] Setup austo start..." 
-pkg install termux-api -y
+echo "[+] Setup storage..."
+termux-setup-storage
+
+echo "[+] Setup auto start..."
 
 mkdir -p ~/.termux/boot
 
 cat > ~/.termux/boot/start.sh <<EOF
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Start runit services
-chmod +x \$HOME/services/*/run
+sleep 5
 runsvdir \$HOME/services &
 EOF
 
 chmod +x ~/.termux/boot/start.sh
-
-echo "[+] install runit..."
-pkg install runit -y
 
 echo "[+] Running app setups..."
 
@@ -30,5 +28,8 @@ for app in apps/*.sh; do
     echo "[+] Running $app"
     bash "$app"
 done
+
+echo "[+] Fixing permissions..."
+chmod +x ~/services/*/run
 
 echo "[✅ DONE]"
--- start-all.sh
+++ start-all.sh
@@ -1,7 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 echo "[*] Starting runit..."
-chmod +x ~/services/*/run
 
 runsvdir ~/services &
 
