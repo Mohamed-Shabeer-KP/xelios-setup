66b0e0234772f58477956bd0d3a09d2410c002d6
Mohamed Shabeer K P <mohammedshabeerkp@gmail.com>
2026-06-06 21:44:27 +0000

    minor

--- apps/vnc.sh
+++ apps/vnc.sh
@@ -14,7 +14,6 @@ cat > ~/.vnc/xstartup <<EOF
 #!/data/data/com.termux/files/usr/bin/bash
 unset SESSION_MANAGER
 unset DBUS_SESSION_BUS_ADDRESS
-exec openbox-session
 EOF
 
 chmod +x ~/.vnc/xstartup
