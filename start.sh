23125ba8ced50dcd8c1f3d1f73c2fcea2b6726f3
Mohamed Shabeer K P <mohammedshabeerkp@gmail.com>
2026-06-06 22:08:57 +0000

    minor

--- start.sh
+++ start.sh
@@ -8,6 +8,6 @@ if [ -d "$HOME/xelios-setup/services" ]; then
     find "$HOME/xelios-setup/services" -type f -name run -exec chmod +x {} \;
 fi
 
-runsvdir ~/xelios-setup/services &
+runsvdir ~/xelios-setup/services 
 
 echo "[✅] All services running!"
