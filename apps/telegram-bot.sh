1a08b7da529962f2fb8b5919ad1e85347fa41a49
Mohamed Shabeer K P <mohammedshabeerkp@gmail.com>
2026-06-07 15:48:48 +0000

    MINOR

--- apps/telegram-bot.sh
+++ apps/telegram-bot.sh
@@ -4,14 +4,16 @@ pip install python-telegram-bot
 
 echo "[+] Setting up environment variables..."
 
-if [ ! -f "$HOME/.env" ]; then
+if [ ! -f "$HOME/xelios-setup/.env" ]; then
     read -p "Enter Telegram Bot Token: " TOKEN
 
-    cat > ~/.env <<EOF
+export TELEGRAM_BOT_TOKEN=$TOKEN
+
+    cat > "$HOME/xelios-setup/.env" <<EOF
 TELEGRAM_BOT_TOKEN=$TOKEN
 EOF
 
-    echo "[+] Token saved to ~/.env"
+    echo "[+] Token saved to ~/xelios-setup/.env"
 else
-    echo "[+] ~/.env already exists, skipping..."
+    echo "[+] ~/xelios-setup/.env already exists, skipping..."
 fi
