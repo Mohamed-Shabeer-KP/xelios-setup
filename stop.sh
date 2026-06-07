42acd15b0371caade0f858f61c6a9277ba15037f
Mohamed Shabeer K P <mohammedshabeerkp@gmail.com>
2026-06-08 00:02:53 +0530

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,5 +1,8 @@
-pkill runsv
-pkill runsvdir
+pkill -9 runsv
+pkill -9 runsvdir
+pkill Xtigervnc
+pkill Xvnc
+rm -f $PREFIX/tmp/.X1-lock
 rm -rf /data/data/com.termux/files/usr/tmp/.X11-unix/*
 rm -f /tmp/.X1-lock
 rm -f /tmp/.X11-unix/X1
