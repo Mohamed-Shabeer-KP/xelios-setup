ee3068e0731a683cdced8b1980a332e46526ff90
Mohamed Shabeer K P <mohammedshabeerkp@gmail.com>
2026-06-13 22:36:55 +0530

    Update aria2.sh

--- apps/aria2.sh
+++ apps/aria2.sh
@@ -2,10 +2,16 @@ echo "[+] Installing aria2..."
 pkg install -y aria2
 
 mkdir -p "$HOME/.aria2"
+touch -p "$HOME/.aria2/aria2.session"
 
 cat > "$HOME/.aria2/aria2.conf" <<EOF
 dir=$HOME/storage/shared/Download
 enable-rpc=true
 rpc-listen-all=true
 rpc-allow-origin-all=true
+input-file=$HOME/.aria2/aria2.session
+save-session=$HOME/.aria2/aria2.session
+save-session-interval=60
+force-save=true
+continue=true
 EOF
